19ce7a70c640422f5afbc6a9e623da7c39d514fb
Angela Sipes <angela.m.sipes73@gmail.com>
2021-06-01 11:02:41 -0500

    initail commit

---
+++ js/external.js
@@ -0,0 +1,18 @@
+"use strict";
+//... rest of javascript code
+
+
+alert("Hello World!");
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
+alert("Welcome to my Website");
+
+var favoriteColor = prompt ("What is your favorite color?");
+alert("Great, " + favoriteColor + " is my favorite color too!");
+
+
+
+
+
+
